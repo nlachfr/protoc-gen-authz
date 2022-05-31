4183fbe754a01d4c8e538490f093edaeb213d6ee
Neakxs <37744447+Neakxs@users.noreply.github.com>
2022-05-31 19:18:06 +0200

    Add TypeFromFunctionType converter

--- authorize/authz.go
+++ authorize/authz.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 
 	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/checker/decls"
 	"github.com/google/cel-go/common/types"
+	v1alpha1 "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
@@ -38,6 +40,43 @@ func AuthorizationContextFromContext(ctx context.Context) *AuthorizationContext 
 		}
 	}
 	return res
+}
+
+func TypeFromFunctionType(t *FileRule_Overloads_Function_Type) *v1alpha1.Type {
+	switch v := t.Type.(type) {
+	case *FileRule_Overloads_Function_Type_Primitive_:
+		switch v.Primitive {
+		case FileRule_Overloads_Function_Type_BOOL:
+			return decls.Bool
+		case FileRule_Overloads_Function_Type_INT:
+			return decls.Int
+		case FileRule_Overloads_Function_Type_UINT:
+			return decls.Uint
+		case FileRule_Overloads_Function_Type_DOUBLE:
+			return decls.Double
+		case FileRule_Overloads_Function_Type_BYTES:
+			return decls.Bytes
+		case FileRule_Overloads_Function_Type_STRING:
+			return decls.String
+		case FileRule_Overloads_Function_Type_DURATION:
+			return decls.Duration
+		case FileRule_Overloads_Function_Type_TIMESTAMP:
+			return decls.Timestamp
+		case FileRule_Overloads_Function_Type_ERROR:
+			return decls.Error
+		case FileRule_Overloads_Function_Type_DYN:
+			return decls.Dyn
+		case FileRule_Overloads_Function_Type_ANY:
+			return decls.Any
+		}
+	case *FileRule_Overloads_Function_Type_Object:
+		return decls.NewObjectType(v.Object)
+	case *FileRule_Overloads_Function_Type_Array_:
+		return decls.NewListType(TypeFromFunctionType(v.Array.Type))
+	case *FileRule_Overloads_Function_Type_Map_:
+		return decls.NewMapType(TypeFromFunctionType(v.Map.Key), TypeFromFunctionType(v.Map.Value))
+	}
+	return decls.Null
 }
 
 type AuthzInterceptor interface {
