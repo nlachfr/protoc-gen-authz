dafa949c15b9e5ea98eccd4de6a3c5bd44a42367
Neakxs <37744447+Neakxs@users.noreply.github.com>
2022-06-01 22:02:29 +0200

    Add TypeFromOverloadType tests

--- authorize/authz_test.go
+++ authorize/authz_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/google/cel-go/cel"
 	"github.com/google/cel-go/checker/decls"
 	"github.com/google/go-cmp/cmp"
+	v1alpha1 "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/metadata"
@@ -70,6 +71,166 @@ func TestAuthorizationContextFromContext(t *testing.T) {
 			got := AuthorizationContextFromContext(tt.In)
 			if !cmp.Equal(got, tt.Want, protocmp.Transform()) {
 				t.Errorf("want %v, got %v", tt.Want, got)
+			}
+		})
+	}
+}
+
+func TestTypeFromOverloadType(t *testing.T) {
+	tests := []struct {
+		Name string
+		In   *FileRule_Overloads_Type
+		Out  *v1alpha1.Type
+	}{
+		{
+			Name: "Primitive bool",
+			In: &FileRule_Overloads_Type{
+				Type: &FileRule_Overloads_Type_Primitive_{
+					Primitive: FileRule_Overloads_Type_BOOL,
+				},
+			},
+			Out: decls.Bool,
+		},
+		{
+			Name: "Primitive int",
+			In: &FileRule_Overloads_Type{
+				Type: &FileRule_Overloads_Type_Primitive_{
+					Primitive: FileRule_Overloads_Type_INT,
+				},
+			},
+			Out: decls.Int,
+		},
+		{
+			Name: "Primitive uint",
+			In: &FileRule_Overloads_Type{
+				Type: &FileRule_Overloads_Type_Primitive_{
+					Primitive: FileRule_Overloads_Type_UINT,
+				},
+			},
+			Out: decls.Uint,
+		},
+		{
+			Name: "Primitive double",
+			In: &FileRule_Overloads_Type{
+				Type: &FileRule_Overloads_Type_Primitive_{
+					Primitive: FileRule_Overloads_Type_DOUBLE,
+				},
+			},
+			Out: decls.Double,
+		},
+		{
+			Name: "Primitive bytes",
+			In: &FileRule_Overloads_Type{
+				Type: &FileRule_Overloads_Type_Primitive_{
+					Primitive: FileRule_Overloads_Type_BYTES,
+				},
+			},
+			Out: decls.Bytes,
+		},
+		{
+			Name: "Primitive string",
+			In: &FileRule_Overloads_Type{
+				Type: &FileRule_Overloads_Type_Primitive_{
+					Primitive: FileRule_Overloads_Type_STRING,
+				},
+			},
+			Out: decls.String,
+		},
+		{
+			Name: "Primitive duration",
+			In: &FileRule_Overloads_Type{
+				Type: &FileRule_Overloads_Type_Primitive_{
+					Primitive: FileRule_Overloads_Type_DURATION,
+				},
+			},
+			Out: decls.Duration,
+		},
+		{
+			Name: "Primitive timestamp",
+			In: &FileRule_Overloads_Type{
+				Type: &FileRule_Overloads_Type_Primitive_{
+					Primitive: FileRule_Overloads_Type_TIMESTAMP,
+				},
+			},
+			Out: decls.Timestamp,
+		},
+		{
+			Name: "Primitive error",
+			In: &FileRule_Overloads_Type{
+				Type: &FileRule_Overloads_Type_Primitive_{
+					Primitive: FileRule_Overloads_Type_ERROR,
+				},
+			},
+			Out: decls.Error,
+		},
+		{
+			Name: "Primitive dyn",
+			In: &FileRule_Overloads_Type{
+				Type: &FileRule_Overloads_Type_Primitive_{
+					Primitive: FileRule_Overloads_Type_DYN,
+				},
+			},
+			Out: decls.Dyn,
+		},
+		{
+			Name: "Primitive any",
+			In: &FileRule_Overloads_Type{
+				Type: &FileRule_Overloads_Type_Primitive_{
+					Primitive: FileRule_Overloads_Type_ANY,
+				},
+			},
+			Out: decls.Any,
+		},
+		{
+			Name: "Object",
+			In: &FileRule_Overloads_Type{
+				Type: &FileRule_Overloads_Type_Object{
+					Object: "object",
+				},
+			},
+			Out: decls.NewObjectType("object"),
+		},
+		{
+			Name: "Array",
+			In: &FileRule_Overloads_Type{
+				Type: &FileRule_Overloads_Type_Array_{
+					Array: &FileRule_Overloads_Type_Array{
+						Type: &FileRule_Overloads_Type{
+							Type: &FileRule_Overloads_Type_Primitive_{
+								Primitive: FileRule_Overloads_Type_BOOL,
+							},
+						},
+					},
+				},
+			},
+			Out: decls.NewListType(decls.Bool),
+		},
+		{
+			Name: "Map",
+			In: &FileRule_Overloads_Type{
+				Type: &FileRule_Overloads_Type_Map_{
+					Map: &FileRule_Overloads_Type_Map{
+						Key: &FileRule_Overloads_Type{
+							Type: &FileRule_Overloads_Type_Primitive_{
+								Primitive: FileRule_Overloads_Type_STRING,
+							},
+						},
+						Value: &FileRule_Overloads_Type{
+							Type: &FileRule_Overloads_Type_Primitive_{
+								Primitive: FileRule_Overloads_Type_STRING,
+							},
+						},
+					},
+				},
+			},
+			Out: decls.NewMapType(decls.String, decls.String),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			res := TypeFromOverloadType(tt.In)
+			if !cmp.Equal(res, tt.Out, protocmp.Transform()) {
+				t.Errorf("want %v, got %v", tt.Out, res)
 			}
 		})
 	}
