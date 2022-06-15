b94012f8d27e2ea602c6f5962467f4374c6ba96f
Neakxs <37744447+Neakxs@users.noreply.github.com>
2022-06-15 23:18:08 +0200

    Add grpc interceptor tests

--- authorize/authz_test.go
+++ authorize/authz_test.go
@@ -13,64 +13,6 @@ import (
 	"google.golang.org/protobuf/testing/protocmp"
 )
 
-// func TestAuthorizationContextFromContext(t *testing.T) {
-// 	tests := []struct {
-// 		Name string
-// 		In   context.Context
-// 		Want *AuthorizationContext
-// 	}{
-// 		{
-// 			Name: "Default",
-// 			In:   context.Background(),
-// 			Want: &AuthorizationContext{
-// 				Peer: &AuthorizationContext_Peer{},
-// 			},
-// 		},
-// 		{
-// 			Name: "Authorization",
-// 			In:   metadata.NewIncomingContext(context.Background(), metadata.New(map[string]string{"authorization": "Basic user:password"})),
-// 			Want: &AuthorizationContext{
-// 				Peer: &AuthorizationContext_Peer{},
-// 				Metadata: map[string]*AuthorizationContext_MetadataValue{
-// 					"authorization": {
-// 						Values: []string{"Basic user:password"},
-// 					},
-// 				},
-// 			},
-// 		},
-// 		{
-// 			Name: "IP Source",
-// 			In: peer.NewContext(context.Background(), &peer.Peer{
-// 				Addr: &net.IPAddr{IP: net.ParseIP("127.0.0.1")},
-// 			}),
-// 			Want: &AuthorizationContext{
-// 				Peer: &AuthorizationContext_Peer{
-// 					Addr: "127.0.0.1",
-// 				},
-// 			},
-// 		},
-// 		{
-// 			Name: "AuthInfo",
-// 			In: peer.NewContext(context.Background(), &peer.Peer{
-// 				AuthInfo: credentials.TLSInfo{},
-// 			}),
-// 			Want: &AuthorizationContext{
-// 				Peer: &AuthorizationContext_Peer{
-// 					AuthInfo: credentials.TLSInfo{}.AuthType(),
-// 				},
-// 			},
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.Name, func(t *testing.T) {
-// 			got := AuthorizationContextFromContext(tt.In)
-// 			if !cmp.Equal(got, tt.Want, protocmp.Transform()) {
-// 				t.Errorf("want %v, got %v", tt.Want, got)
-// 			}
-// 		})
-// 	}
-// }
-
 func TestTypeFromOverloadType(t *testing.T) {
 	tests := []struct {
 		Name string
--- authorize/interceptors/grpc.go
+++ authorize/interceptors/grpc.go
@@ -33,25 +33,3 @@ func NewGRPCUnaryInterceptor(authzHandler authorize.AuthzInterceptor) grpc.Unary
 		return handler(ctx, req)
 	}
 }
-
-func NewGRPCStreamInterceptor(authzHandler authorize.AuthzInterceptor) grpc.StreamServerInterceptor {
-	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-		ctx := ss.Context()
-		headers := http.Header{}
-		if md, ok := metadata.FromIncomingContext(ctx); ok {
-			for k, v := range md {
-				for _, vv := range v {
-					headers.Add(k, vv)
-				}
-			}
-		}
-		if p, ok := peer.FromContext(ctx); ok && p.Addr != nil {
-			headers.Add("forwarded", fmt.Sprintf("for=%s", p.Addr.String()))
-			headers.Add("x-forwarded-for", p.Addr.String())
-		}
-		if err := authzHandler.Authorize(ctx, info.FullMethod, headers, nil); err != nil {
-			return status.Error(codes.PermissionDenied, err.Error())
-		}
-		return handler(srv, ss)
-	}
-}
